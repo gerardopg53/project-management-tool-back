8b682e5a1bc808f991c1e4a25da5e928115155f4
Gerardo Perez <gerris909@Gerardos-MBP.attlocal.net>
2021-02-18 16:49:08 -0700

    springboot boilerplate added

---
+++ src/test/java/com/gerardoperez/ppmtool/ProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardoperez.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
